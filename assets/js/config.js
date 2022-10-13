80318d8be4e3f3c797db22a7b7010c18fcc1a3b1
yangqinlong <2214409512@qq.com>
2022-10-13 17:50:36 +0800

    1

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,3 +2,4 @@ var baseUrl = "https://api.stock-plouto.com";
 // var baseUrl =   "http://192.168.1.11:8080"
 var arrList = ["Stripe", "PayPal"];
 var productData = null;//当前商品信息
+var stripeKey="pk_live_51L8IpsIzNzEUKhl8uXHCShyfEXTUAcetBsKbaG690FCXWGcBlFWoxbRdqsKjvKjh2k1WSkRKXYhRl3iZVqLjwZSI00fmzBBSTm"
--- assets/js/product.js
+++ assets/js/product.js
@@ -1,5 +1,5 @@
 var tokens = JSON.parse(localStorage.getItem("ploutoUserInfo")) || "";
-const stripe = Stripe("pk_live_51L8IpsIzNzEUKhl8uXHCShyfEXTUAcetBsKbaG690FCXWGcBlFWoxbRdqsKjvKjh2k1WSkRKXYhRl3iZVqLjwZSI00fmzBBSTm");
+const stripe = Stripe(stripeKey);
 const items = [{ id: "prod_LxQP3nkuvcykMZ" }];
 var count = 0;//当前选择的支付方式，0=stripe，1=paypal
 $(".buyNowProduct").click(() => {
